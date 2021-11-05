820355233911ee449b06a25a11d460769818d108
Dominick Simone <domsimone123@gmail.com>
2021-11-05 13:14:44 -0400

    adding front end mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($username: String!, $email: String!, $password: String!) {
+        addUser(username: $username, email: $email, password: $password) {
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBooks($authors: [String], $description: String, $bookId: String, $image: String, $link: String, $title: String) {
+    saveBooks(authors: $authors, description: $description, bookId: $bookId, image: $image, link: $link, title: $title) {
+        _id
+        username
+        savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+        
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+        _id
+        savedBooks {
+            authors
+            description
+            bookId
+            image
+            link
+            title
+        }
+    }
+}
+`;
